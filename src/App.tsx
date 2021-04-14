598e3b9b50c98ffbd5b387c908cee9ced8f4b4d1
okb-okb <kosukeokabe1998@gmail.com>
2021-04-14 12:14:54 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from "react";
+import styled from "styled-components";
 
-function App() {
+const MyTitle = styled.h1`
+  font-size: 2em;
+  text-align: center;
+  color: cornflowerblue;
+`;
+
+const CenteredDiv = styled.div`
+  text-align: center;
+`;
+
+const MyButton = styled.button`
+  background: ${(props) =>
+    props.className === "blue" ? "cornflowerblue" : "white"};
+  color: ${(props) =>
+    props.className === "blue" ? "white" : "cornflowerblue"};
+
+  font-size: 1.5em;
+  margin: 1em;
+  padding: 1em 3em;
+  border: 2px solid cornflowerblue;
+  border-radius: 3px;
+`;
+
+const OverRidedButton = styled(MyButton)`
+  background: white;
+  color: lightseagreen;
+  border-color: lightseagreen;
+`;
+
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MyTitle>Welcome to my page!!</MyTitle>
+      <CenteredDiv>
+        <MyButton>Skills</MyButton>
+        <MyButton className="blue">Works</MyButton>
+        <OverRidedButton>Links</OverRidedButton>
+      </CenteredDiv>
+    </>
   );
-}
+};
 
 export default App;
